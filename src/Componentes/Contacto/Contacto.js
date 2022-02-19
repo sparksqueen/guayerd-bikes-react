87115c7a2d611eaeb1fb4488f430041983328244
Maria <marixi@outlook.com.ar>
2022-02-19 01:01:09 -0300

    Validacion de Datos (Listo)

--- src/Componentes/Contacto/Contacto.js
+++ src/Componentes/Contacto/Contacto.js
@@ -2,46 +2,88 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./contacto.css";
 
+(function () {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+
 const Contacto = () => {
   return (
     <div className="container">
       <h2 className="pt-4">Contacto</h2>
       <div className="contacto row justify-content-center py-4 px-4">
-        <form class="row g-3">
+        <form class="row g-3 needs-validation" novalidate>
           <div class="row">
             <div class="col-md-6">
-              <label for="exampleFormControlInput1" class="form-label">
+              <label for="validationCustom01" class="form-label">
                 Nombre
               </label>
               <input
                 type="text"
                 class="form-control"
-                placeholder="José"
-                aria-label="First name"
+                id="validationCustom01"
+                placeholder="Mario"
+                required
               />
+              <div class="valid-feedback">Bien!</div>
+              <div class="invalid-feedback">
+                Ningún campo obligatorio debe estar vacío.
+              </div>
             </div>
             <div class="col-md-6">
-              <label for="exampleFormControlInput1" class="form-label">
+              <label for="validationCustom02" class="form-label">
                 Apellido
               </label>
               <input
                 type="text"
                 class="form-control"
-                placeholder="Perez"
-                aria-label="Last name"
+                id="validationCustom02"
+                placeholder="Ruiz"
+                required
               />
+              <div class="valid-feedback">Bien!</div>
+              <div class="invalid-feedback">
+                Ningún campo obligatorio debe estar vacío.
+              </div>
             </div>
           </div>
           <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">
+            <label for="validationDefaultUsername" class="form-label">
               Email
             </label>
-            <input
-              type="email"
-              class="form-control"
-              id="exampleFormControlInput1"
-              placeholder="name@ejemplo.com"
-            />
+            <div class="input-group has-validation">
+              <input
+                type="email"
+                class="form-control"
+                id="validationDefaultUsername"
+                placeholder="name@ejemplo.com"
+                required
+              />
+              <div class="invalid-feedback">
+                Ningún campo obligatorio debe estar vacío.
+              </div>
+              <div class="invalid-feedback">
+                El campo de email debe ser valido.
+              </div>
+            </div>
           </div>
           <div className="col-12">
             <label for="inputAddress" class="form-label">
@@ -51,8 +93,13 @@ const Contacto = () => {
               type="text"
               className="form-control"
               id="inputAddress"
-              placeholder="1234 Main St"
+              placeholder="Callao 2000"
+              required
             />
+            <div class="valid-feedback">Bien!</div>
+            <div class="invalid-feedback">
+              Ningún campo obligatorio debe estar vacío.
+            </div>
           </div>
           <select class="form-select" aria-label="Default select example">
             <option selected>Sector</option>
@@ -61,18 +108,26 @@ const Contacto = () => {
             <option value="3">Compras</option>
           </select>
           <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">
+            <label for="validationTextarea" class="form-label">
               Mensaje
             </label>
             <textarea
+              type="text"
               class="form-control"
-              id="exampleFormControlTextarea1"
-              rows="3"
+              id="validationTextarea"
+              placeholder="Escriba su mensaje aquí"
+              required
             ></textarea>
           </div>
+          <div class="invalid-feedback">
+            El campo de comentarios debe tener más que 5 caracteres.
+          </div>
+          <div class="invalid-feedback">
+            Ningún campo obligatorio debe estar vacío.
+          </div>
           <div className="col-12">
-            <button type="submit" className="btn btn-primary">
-              Sign in
+            <button class="btn btn-primary" type="submit">
+              Enviar
             </button>
           </div>
         </form>
